a0ca3d855bcdef5cc1628a6ae0303b924b00cfb9
Alan F <alan_ferreyra@live.com>
2023-12-16 20:58:05 -0300

    correção

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-$("form").on("submit", function (e) {
-  e.preventDefault();
+$(document).ready(function () {
+  $("form").on("submit", function (e) {
+    e.preventDefault();
 
-  const NovaTarefa = $(`#nome-tarefa`).val();
+    const NovaTarefa = $(`#nome-tarefa`).val();
 
-  $(`<li>${NovaTarefa}</li>`).appendTo("ul");
-
-  $(document).ready(function () {
-    $("ul").on("click", "li", function () {
-      $(this).toggleClass("completed");
-    });
+    $(`<li>${NovaTarefa}</li>`).appendTo("ul");
 
     $(`#nome-tarefa`).val("");
+  });
+
+  $("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
   });
 });
